3228975400fb559f4820cdd6fa3694b281c38503
Aaron Janes <Aaron.Janes@gmail.com>
2021-04-15 16:02:26 -0600

    add mobile styles

--- src/pages/LifeSentenceCycles.jsx
+++ src/pages/LifeSentenceCycles.jsx
@@ -4,25 +4,27 @@ import logo from "../assets/LSC2.png";
 import logo1 from "../assets/LSC3.png";
 import logo2 from "../assets/LSC4.png";
 
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
 import { EnvelopeOpenFill, Globe2, Phone } from "react-bootstrap-icons/";
 
 export const LifeSentenceCycles = () => {
   return (
-    <div>
-      <div className="wrapper-lsc">
-        <img src={logo1} alt="" />
-        <img src={logo} alt="" />
-        <img src={logo2} alt="" />
-        <div className="contact">
-          <h3>
-            &nbsp;
-            <br /> Edmonton Alberta fully certified Harley Davidson repair,
-            service and restoration. <br />
-            Journeyman motorcycle mechanics and auto Harley Davidson Specialists
-            <br />
-            1903- Current Models Service - Sales - Apparel Journeyman Mechanics
-            Journeyman Auto body Certified & Warrantied body.
-          </h3>
+    <div className="wrapper-lsc">
+      <img src={logo1} className="image" alt="" />
+      <img src={logo} className="image" alt="" />
+      <img src={logo2} className="image" alt="" />
+      <div className="contact">
+        <h3>
+          &nbsp;
+          <br /> Edmonton Alberta fully certified Harley Davidson repair,
+          service and restoration. <br />
+          Journeyman motorcycle mechanics and auto Harley Davidson Specialists
+          <br />
+          1903- Current Models Service - Sales - Apparel Journeyman Mechanics
+          Journeyman Auto body Certified & Warrantied body.
+        </h3>
+        <div className="contact-info">
           <ul>
             <li>
               <Phone />
