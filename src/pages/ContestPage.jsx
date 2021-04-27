62f1e0e2e8c3dacf9327e6b38ad5546008fa939f
Aaron Janes <Aaron.Janes@gmail.com>
2021-04-27 11:37:54 -0600

    change background for contest

--- src/pages/ContestPage.jsx
+++ src/pages/ContestPage.jsx
@@ -7,10 +7,10 @@ export const ContestPage = () => {
   return (
     <>
       <div className="contest-container">
-        <h3>
+        {/* <h3>
           One Lucky Winner <br /> Will receive a wristband for free entry into
           the camp-out & a sticker pack
-        </h3>
+        </h3> */}
       </div>
       <ContestComponent />
     </>
