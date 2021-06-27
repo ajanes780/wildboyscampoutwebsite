eb6c599e858833fba2aac807ba09104aee28104c
Aaron Janes <aaron.janes@gmail.com>
2021-06-27 09:16:25 -0600

    added  charity

--- src/components/card/CardComponent.jsx
+++ src/components/card/CardComponent.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { Card, Button } from "react-bootstrap";
-import { Link, useHistory } from "react-router-dom";
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+import { Link, useHistory } from 'react-router-dom';
 
-import "./cardComponentStyle.scss";
+import './cardComponentStyle.scss';
 
 export const CardComponent = ({
   image,
@@ -14,17 +14,18 @@ export const CardComponent = ({
 }) => {
   const history = useHistory();
   return (
-    <Card id="store-card" style={{ width: "18rem", margin: "10px" }}>
-      <Card.Img variant="top" src={image} Style={{ Height: "100px" }} />
+    <Card id='store-card' style={{ width: '25rem', margin: '1rem' }}>
+      <Card.Img variant='top' src={image} Style={{ Height: '100px' }} />
       <Card.Body>
-        <Card.Title>{title}</Card.Title>
+        <Card.Title style={{ fontWeight: 'bold' }}>{title}</Card.Title>
         <Card.Text>{bio}</Card.Text>
         <Button
-          onClick={() => history.push(linkUrl)}
-          id="mybtn"
-          variant="warning"
+          // onClick={() => history.push(linkUrl)}
+          id='dawson'
+          variant='warning'
         >
-          <Link to={linkUrl}>{buttonMsg}</Link>
+          <a href={linkUrl}> {buttonMsg}</a>
+          {/* <Link to={linkUrl}>{buttonMsg}</Link> */}
         </Button>
       </Card.Body>
     </Card>
